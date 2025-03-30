19e4feef9ff0249aff8888970ce97690998e85a2
Wummp <leo.garrels@gmail.com>
2025-03-30 13:21:12 +0200

    pickle scheint zu funktionieren. Löschen wäre irgendwann ein nächster Schritt.

--- main.py
+++ main.py
@@ -3,31 +3,35 @@ import pickle
 # variables
 menueingabe = ""
 setupeingabe = ""
-
+wortliste = []
 # functions
 
 def menu():
     global menueingabe
-    menueingabe = input("MENU: (1) Start. (2) Setup. ")
+    menueingabe = input("MENU: START or SETUP. ").lower()
 
 def setup():
     global setupeingabe
-    setupeingabe = input("SETUP: Gebe ein Wort ein, dass zur Liste hinzugefügt werden soll. START zum Starten. END um zum Menu zurückzukehren. ").lower()
+    setupeingabe = input("SETUP: Gebe ein Wort ein, dass zur Liste hinzugefügt werden soll. \n" \
+    "... LIST um Wortliste anzuzeigen. START zum Starten. END um zum Menu zurückzukehren. ").lower()
+
+# def start():
 
 # main
-
-with open("wortliste.pkl", "rb") as f:
-    wortliste = pickle.load(f)
-    f.close()
+try:
+    with open("wortliste.pkl", "rb") as f:
+        wortliste = pickle.load(f)
+except:
+    print(" ")
 
 print("Willkommen. ")
 while True:
     menu()
     match menueingabe:
-        case "1":
+        case "start":
             print("Hier solls dann losgehen, aber soweit bin ich noch nicht. ")
             break
-        case "2":
+        case "setup":
             while True:
                 setup()
                 match setupeingabe:
@@ -39,6 +43,10 @@ while True:
                     case "start":
                         print("Hier soll dann losgehen, aber soweit bin ich noch nicht. ")
                         break
+                    case "list":
+                        for item in wortliste:
+                            print("- " + item)
+                            continue
                     case _ if setupeingabe.isalpha() == False:
                         print("FEHLER: bitte nur ein einzelnes Wort eingeben. ")
                         continue
@@ -48,7 +56,9 @@ while True:
                         for item in wortliste:
                             print("- " + item)
                             continue
+                        with open("wortliste.pkl", "wb") as f:
+                            pickle.dump(wortliste, f)
                         continue
         case _:
-            print("Bitte nur 1 oder 2 eingeben.")
+            print("FEHLER: Bitte nur START oder SETUP eingeben.")
             continue
